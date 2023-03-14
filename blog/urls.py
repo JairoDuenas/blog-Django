05d76a451b0b8a3cc5611fb716afd49ae00d00e7
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-14 10:30:32 -0500

    Estilando definitivo [home, about]

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,6 @@ from myblog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]

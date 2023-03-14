bc51c4fedaa53c1e45b6f3071dfa031ea19eec72
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-14 14:27:03 -0500

    Modularizando [home, about]

--- about/views.py
+++ about/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-  return render(request, 'home/home.html')
-
 def about(request):
   return render(request, 'about/about.html')

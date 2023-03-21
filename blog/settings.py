96279d84730df9f3c962547f31171008bb6985f2
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-21 17:16:43 -0500

    config media

--- blog/settings.py
+++ blog/settings.py
@@ -143,10 +143,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
